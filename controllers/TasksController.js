672d9ca106517101f5278eb655c98dd56fab3d76
Dazuam <dazuam2@gmail.com>
2020-09-11 21:46:18 -0500

    EndofClass

---
+++ controllers/TasksController.js
@@ -0,0 +1,16 @@
+const Task = require('../models/Task');
+
+exports.store = (req, res) => {
+  let task = {};
+  task.description = req.body.description;
+  Task.create(task).then((id) => {
+    console.log('Task created with id: ', id);
+     // if the request is expecting an ajax or json response
+    if (req.xhr || req.headers.accept.indexOf('json') > -1) {
+      Task.find(id).then((task) => res.json(task));
+    } else {
+      res.redirect('/');
+    }
+    //res.redirect('/'); //No se necesita porque AJAX
+  });
+}
