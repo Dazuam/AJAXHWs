c897902765a0f11238286d995bb150f812fee623
Dazuam <dazuam2@gmail.com>
2020-09-18 00:21:47 -0500

    AJAX Add Task Working
    Downloaded Minervatron Base

--- controllers/TasksController.js
+++ controllers/TasksController.js
@@ -5,12 +5,10 @@ exports.store = (req, res) => {
   task.description = req.body.description;
   Task.create(task).then((id) => {
     console.log('Task created with id: ', id);
-     // if the request is expecting an ajax or json response
     if (req.xhr || req.headers.accept.indexOf('json') > -1) {
       Task.find(id).then((task) => res.json(task));
     } else {
       res.redirect('/');
     }
-    //res.redirect('/'); //No se necesita porque AJAX
   });
 }
